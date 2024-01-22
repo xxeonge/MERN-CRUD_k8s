01b7c4ef0dbaace9ae8e4c9eeec241cfe914fe41
xxeonge <xeoyounglee@gmail.com>
2024-01-22 22:33:13 +0900

    Modified

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+const connection_uri= (host) => {
+	return `mongodb://${host}:27017/MERN`;
+};
+
 module.exports = () => {
-	const connection = mongoose
-		.connect(process.env.MONGODB_URI)
-		.then((result) => console.log("Connected to database"))
-		.catch((err) => console.log("could not connect to database"));
+  const connection = mongoose
+    .connect((connection_uri(process.env.MONGODB_HOST)), {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true, 
+    })
+    .then((result) => console.log("Connected to database"))
+    .catch((err) => console.log("Could not connect to database"));
 };
